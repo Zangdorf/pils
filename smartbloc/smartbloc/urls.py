7b12230ea7787efb06b218f0b3c0333a2c7d3d8c
Godefroy GOFFE <godefroygoffe@gmail.com>
2019-11-08 12:50:14 +0100

    links on buttons

--- smartbloc/smartbloc/urls.py
+++ smartbloc/smartbloc/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', include("cover.urls")),
-    #path('admin/', admin.site.urls),
+
+    url(r'^', include("cover.urls")),
+    url(r'^stats/', include("stats.urls")),
+    #url(r'^admin/', admin.site.urls),
 ]
