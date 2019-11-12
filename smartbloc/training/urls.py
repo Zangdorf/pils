04477c4eb23f5419b5a0d057ce1f04809a53c433
Aurelien Martin <aurmartin@pm.me>
2019-11-12 12:59:27 +0100

    Improved main menu design, added training settings, show image when training starts

--- smartbloc/training/urls.py
+++ smartbloc/training/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
 
     url(r'^$', views.training, name='training'),
+    path('showImage', views.showImage),
 
 ]
--- smartbloc/training/views.py
+++ smartbloc/training/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from PIL import Image
 
 # Create your views here.
 def training(request):
     return render(request, "training.html")
+
+def showImage(request):
+    img = Image.open('test.jpg')
+    img.show()
+    return HttpResponse(status=200)
