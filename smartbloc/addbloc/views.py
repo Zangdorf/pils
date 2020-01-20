a1db7681542d6d05e396963d73d4a403ae1bd21b
Aurélien Martin <aurlienmartin@gmail.com>
2020-01-20 18:25:44 +0100

    add: create block page

--- smartbloc/addbloc/views.py
+++ smartbloc/addbloc/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def addbloc(request):
+    if request.POST:
+        print(request.body)
+        return HttpResponse(status=200)
     return render(request, "addbloc.html")
--- smartbloc/training/views.py
+++ smartbloc/training/views.py
@@ -4,12 +4,13 @@ from PIL import Image
 
 # Create your views here.
 def training(request):
-    img = Image.open('training/static/img/bloc_default.png')
-    img.show()
+    # img = Image.open('training/static/img/bloc_default.png')
+    # img.show()
     return render(request, "training.html")
 
 def showImage(request):
     print(request.GET)
-    img = Image.open('training/static/img/bloc_{}.png'.format(request.GET['difficulty']))
+    # img = Image.open('training/static/img/bloc_{}.png'.format(request.GET['difficulty']))
+    img = Image.open('training/static/img/test.png')
     img.show()
     return HttpResponse(status=200)
